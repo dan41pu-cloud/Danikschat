b6f1c8961226e1a60a135adb06e2c67298989e4c
dan41pu-cloud <dan41pu@gmail.com>
2025-11-17 21:40:08 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,54 +1,125 @@
 const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const fs = require("fs");
+const path = require("path");
+
 const app = express();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "*", methods: ["GET","POST"] }
+});
 
 app.use(express.static(__dirname));
 
-app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
+const messagesFile = path.join(__dirname, "messages.json");
+const usersFile = path.join(__dirname, "users.json");
+const securityLogFile = path.join(__dirname, "security.log");
 
-// ===== ЧАТ + ВИДЕОЧАТ =====
-let videoRoom = [];
+function loadData(file, def = []) {
+  if(fs.existsSync(file)) return JSON.parse(fs.readFileSync(file,"utf8"));
+  fs.writeFileSync(file, JSON.stringify(def,null,2));
+  return def;
+}
 
-io.on("connection", socket => {
+function saveData(file, data) {
+  fs.writeFileSync(file, JSON.stringify(data,null,2));
+}
 
-  // простой чат
-  socket.on("chat_message", msg => io.emit("chat_message", msg));
+function logSecurity(message) {
+  fs.appendFile(securityLogFile, `[${new Date().toISOString()}] ${message}\n`, () => {});
+}
 
-  // видеочат
-  socket.on("join-video-room", () => {
-    videoRoom.push(socket);
+let messages = loadData(messagesFile);
+let users = loadData(usersFile);
+let activeUsers = new Set();
 
-    if (videoRoom.length === 1) {
-      socket.emit("waiting");
-    } else if (videoRoom.length === 2) {
-      const [first, second] = videoRoom;
-      first.emit("ready");
-      second.emit("ready");
-    } else {
-      socket.emit("waiting");
+// Видеокомната (только 2 пользователя)
+let videoRoomUsers = [];
+
+io.on("connection", (socket) => {
+  console.log("🔗 Новый пользователь");
+
+  // ------------------ РЕГИСТРАЦИЯ ------------------
+  socket.on("register", ({ username, password }) => {
+    if(!username || !password)
+      return socket.emit("registerError", "Введите имя и пароль");
+
+    if(users.some(u => u.username.toLowerCase() === username.toLowerCase()))
+      return socket.emit("registerError", "Имя занято");
+
+    const isFirst = users.length === 0;
+
+    users.push({ username, password, admin: isFirst });
+    saveData(usersFile, users);
+
+    socket.emit("registerSuccess", "Успешно! Теперь войдите.");
+  });
+
+  // ------------------ ВХОД ------------------
+  socket.on("login", ({ username, password }) => {
+    const user = users.find(u => u.username === username && u.password === password);
+    if(!user) return socket.emit("loginError", "Неверные данные");
+
+    if(activeUsers.has(username)) {
+      socket.emit("loginError", "Этот пользователь уже онлайн");
+      logSecurity(`Попытка входа ${username}: аккаунт уже активен`);
+      return;
+    }
+
+    socket.username = username;
+    socket.admin = !!user.admin;
+    activeUsers.add(username);
+
+    socket.emit("loginSuccess", { username, admin: user.admin, messages });
+
+    logSecurity(`${username} вошёл`);
+  });
+
+  // ------------------ СООБЩЕНИЯ ------------------
+  socket.on("chat message", msg => {
+    const time = new Date().toLocaleTimeString();
+    const m = { ...msg, time };
+    messages.push(m);
+    saveData(messagesFile, messages);
+    io.emit("chat message", m);
+  });
+
+  // ------------------ ИЗОБРАЖЕНИЯ ------------------
+  socket.on("chat image", msg => {
+    const time = new Date().toLocaleTimeString();
+    const m = { ...msg, time };
+    messages.push(m);
+    saveData(messagesFile, messages);
+    io.emit("chat image", m);
+  });
+
+  // ------------------ ВИДЕОЧАТ ------------------
+  socket.on("joinVideo", () => {
+    videoRoomUsers.push(socket.id);
+
+    if (videoRoomUsers.length === 2) {
+      io.to(videoRoomUsers[0]).emit("videoReady");
+      io.to(videoRoomUsers[1]).emit("videoReady");
     }
   });
 
-  socket.on("offer", data => {
-    const other = videoRoom.find(s => s.id !== socket.id);
-    if(other) other.emit("offer", data);
+  socket.on("leaveVideo", () => {
+    videoRoomUsers = videoRoomUsers.filter(id => id !== socket.id);
+    socket.broadcast.emit("videoLeft");
   });
 
-  socket.on("answer", data => {
-    const other = videoRoom.find(s => s.id !== socket.id);
-    if(other) other.emit("answer", data);
-  });
+  socket.on("offer", offer => socket.broadcast.emit("offer", offer));
+  socket.on("answer", ans => socket.broadcast.emit("answer", ans));
+  socket.on("ice", cand => socket.broadcast.emit("ice", cand));
 
-  socket.on("ice", data => {
-    const other = videoRoom.find(s => s.id !== socket.id);
-    if(other) other.emit("ice", data);
-  });
-
+  // ------------------ ОТКЛЮЧЕНИЕ ------------------
   socket.on("disconnect", () => {
-    videoRoom = videoRoom.filter(s => s.id !== socket.id);
+    activeUsers.delete(socket.username);
+    videoRoomUsers = videoRoomUsers.filter(id => id !== socket.id);
+    socket.broadcast.emit("videoLeft");
+    logSecurity(`${socket.username} вышел`);
   });
-
 });
 
-http.listen(3000, () => console.log("🚀 Сервер запущен на порту 3000"));
+server.listen(3000, () => console.log("🚀 Сервер запущен на 3000"));
