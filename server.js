1debf6d3f4d2b929f6f45337b24ac0ee4f2fdeda
dan41pu-cloud <dan41pu@gmail.com>
2025-12-14 09:48:16 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,20 +3,20 @@ const http = require("http");
 const { Server } = require("socket.io");
 const fs = require("fs");
 const path = require("path");
+const https = require("https");
 
 const app = express();
 const server = http.createServer(app);
+
 const io = new Server(server, {
   cors: { origin: "*", methods: ["GET", "POST"] }
 });
 
 app.use(express.static(__dirname));
 
-/* ===== FILES ===== */
 const usersFile = path.join(__dirname, "users.json");
 const messagesFile = path.join(__dirname, "messages.json");
 
-/* ===== HELPERS ===== */
 function load(file, def = []) {
   if (!fs.existsSync(file)) fs.writeFileSync(file, JSON.stringify(def, null, 2));
   return JSON.parse(fs.readFileSync(file, "utf8"));
@@ -25,15 +25,56 @@ function save(file, data) {
   fs.writeFileSync(file, JSON.stringify(data, null, 2));
 }
 
-/* ===== DATA ===== */
 let users = load(usersFile);
 let messages = load(messagesFile);
-let missedCalls = [];
 
-const sockets = {}; // username -> socket
+const sockets = {}; // username → socket
+
+/* ===== XIRSYS ICE ===== */
+
+const XIRSYS_USER = "daniil";
+const XIRSYS_TOKEN = "787333b8-cedf-11f0-bad6-0242ac130003";
+const XIRSYS_PATH = "/_turn/MyFirstApp";
+
+function getXirsys() {
+  return new Promise(resolve => {
+    const body = JSON.stringify({ format: "ice" });
+
+    const req = https.request({
+      host: "global.xirsys.net",
+      path: XIRSYS_PATH,
+      method: "PUT",
+      headers: {
+        "Authorization":
+          "Basic " + Buffer.from(`${XIRSYS_USER}:${XIRSYS_TOKEN}`).toString("base64"),
+        "Content-Type": "application/json",
+        "Content-Length": body.length
+      }
+    }, res => {
+      let data = "";
+      res.on("data", c => data += c);
+      res.on("end", () => {
+        try {
+          resolve(JSON.parse(data).v.iceServers);
+        } catch {
+          resolve([{ urls: "stun:stun.l.google.com:19302" }]);
+        }
+      });
+    });
+
+    req.on("error", () => resolve([{ urls: "stun:stun.l.google.com:19302" }]));
+    req.write(body);
+    req.end();
+  });
+}
 
 /* ===== SOCKET.IO ===== */
+
 io.on("connection", socket => {
+
+  socket.on("request-ice", async () => {
+    socket.emit("ice-servers", await getXirsys());
+  });
 
   /* === REGISTRATION === */
   socket.on("register", ({ username, password }) => {
@@ -63,27 +104,22 @@ io.on("connection", socket => {
     socket.admin = user.admin;
     sockets[username] = socket;
 
-    // пропущенные звонки
-    const userMissed = missedCalls.filter(c => c.to === username);
-    missedCalls = missedCalls.filter(c => c.to !== username);
-
     socket.emit("loginSuccess", {
       username,
       admin: user.admin,
-      users: users.map(u => u.username),
+      users: users.map(u => u.username), // ВСЕ зарегистрированные
       messages: messages.filter(
         m => m.from === username || m.to === username
-      ),
-      missedCalls: userMissed
+      )
     });
   });
 
-  /* === GET ALL REGISTERED USERS === */
+  /* === ВСЕ ЗАРЕГИСТРИРОВАННЫЕ ПОЛЬЗОВАТЕЛИ === */
   socket.on("get-all-users", () => {
     socket.emit("all-users", users.map(u => u.username));
   });
 
-  /* === PRIVATE MESSAGE (ONLINE + OFFLINE) === */
+  /* === PRIVATE CHAT (ONLINE + OFFLINE) === */
   socket.on("chat-private", msg => {
     const message = {
       ...msg,
@@ -99,17 +135,38 @@ io.on("connection", socket => {
     }
   });
 
-  /* === CALL USER === */
+  /* === VIDEO CALL === */
   socket.on("call-user", ({ target }) => {
     if (sockets[target]) {
       sockets[target].emit("incoming-call", {
         from: socket.username
       });
-    } else {
-      missedCalls.push({
+    }
+  });
+
+  socket.on("webrtc-offer", ({ target, offer }) => {
+    if (sockets[target]) {
+      sockets[target].emit("webrtc-offer", {
         from: socket.username,
-        to: target,
-        time: new Date().toLocaleString()
+        offer
+      });
+    }
+  });
+
+  socket.on("webrtc-answer", ({ target, answer }) => {
+    if (sockets[target]) {
+      sockets[target].emit("webrtc-answer", {
+        from: socket.username,
+        answer
+      });
+    }
+  });
+
+  socket.on("webrtc-candidate", ({ target, candidate }) => {
+    if (sockets[target]) {
+      sockets[target].emit("webrtc-candidate", {
+        from: socket.username,
+        candidate
       });
     }
   });
@@ -120,5 +177,5 @@ io.on("connection", socket => {
 });
 
 server.listen(3000, () => {
-  console.log("Server running: http://localhost:3000");
+  console.log("Server running http://localhost:3000");
 });
