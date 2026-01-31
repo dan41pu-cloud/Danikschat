9ea1f5f469035c1b4ae2bfbb5a65dac141007339
dan41pu-cloud <dan41pu@gmail.com>
2026-01-31 11:09:25 +0100

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,22 @@
-self.addEventListener("install", () => {
-  console.log("SW installed");
+self.addEventListener("push", event => {
+  const data = event.data?.json() || {};
+
+  const title = data.title || "Новое сообщение";
+  const options = {
+    body: data.body || "Вам пришло сообщение",
+    icon: "/icon-192.png",
+    badge: "/icon-192.png",
+    data: data.url || "/"
+  };
+
+  event.waitUntil(
+    self.registration.showNotification(title, options)
+  );
 });
 
-self.addEventListener("activate", () => {
-  console.log("SW activated");
+self.addEventListener("notificationclick", event => {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow(event.notification.data || "/")
+  );
 });
