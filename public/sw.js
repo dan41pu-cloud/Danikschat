1bb964eb042eed94decb447a39d787dce48f6ea4
dan41pu-cloud <dan41pu@gmail.com>
2026-02-02 18:31:21 +0100

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,22 +1,23 @@
 self.addEventListener("push", event => {
-  const data = event.data?.json() || {};
+  if (!event.data) return;
 
-  const title = data.title || "Новое сообщение";
-  const options = {
-    body: data.body || "Вам пришло сообщение",
-    icon: "/icon-192.png",
-    badge: "/icon-192.png",
-    data: data.url || "/"
-  };
+  const data = event.data.json();
 
   event.waitUntil(
-    self.registration.showNotification(title, options)
+    self.registration.showNotification(data.title || "Новое сообщение", {
+      body: data.body,
+      icon: "/icon-192.png",
+      badge: "/icon-192.png",
+      data: {
+        url: data.url || "/"
+      }
+    })
   );
 });
 
 self.addEventListener("notificationclick", event => {
   event.notification.close();
   event.waitUntil(
-    clients.openWindow(event.notification.data || "/")
+    clients.openWindow(event.notification.data.url)
   );
 });
