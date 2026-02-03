cba08cd74668e0059e9da171d9f35a934532eae0
dan41pu-cloud <dan41pu@gmail.com>
2026-02-03 13:22:26 +0100

    Update server.js

--- server.js
+++ server.js
@@ -183,4 +183,6 @@ io.on("connection", socket => {
 
 });
 
-server.listen(3000, () => console.log("✅ Server running http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log("✅ Server running on", PORT));
+
